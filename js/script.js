a98b556273043329950d52ae058986d0ab75cec2
casper91044 <casper91044@mail.ru>
2020-10-17 18:16:53 +0300

    Add Timer

--- js/script.js
+++ js/script.js
@@ -31,4 +31,58 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+    // Timer
+
+    let deadLine = '2020-10-21';
+
+    function getTimeRemaining(endTime) {
+        let total = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((total / 1000) % 60),
+            minutes = Math.floor((total / 1000 / 60) % 60),
+            hours = Math.floor((total / 1000 / 60 / 60) % 24),
+            days = Math.floor((total / (1000 * 60 * 60 * 24)));
+
+        return {
+            'total': total,
+            'days': days,
+            'hour': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+        timeInterval = setInterval(updateClock, 1000)
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hour);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
 });
